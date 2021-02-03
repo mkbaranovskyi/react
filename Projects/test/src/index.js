4dd5aa389ba99fa1dd395fa4fde014b162cda88a
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2021-02-03 04:03:14 +0200

    next lesson

--- Projects/test/src/index.js
+++ Projects/test/src/index.js
@@ -1,64 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
+function FancyBorder(props) {
 	return (
-		<div>
-			<LoginControl />
-			<LoginControl />
+		<div className={'FancyBorder FancyBorder-' + props.color}>
+			{props.children}
 		</div>
 	)
 }
 
-function UserGreeting(props) {
-	return <h2>Welcome back!</h2>
+function WelcomeDialog() {
+	return (
+		<FancyBorder color="blue">
+			<h1 className="Dialog-title">Welcome</h1>
+			<p className="Dialog-message">Thank you for visiting our spacecraft!</p>
+		</FancyBorder>
+	)
 }
 
-function GuestGreeting(props) {
-	return <h2>Please sign in</h2>
-}
-
-function Greeting(props) {
-	if (props.isLoggedIn) {
-		return <UserGreeting />
-	}
-	return <GuestGreeting />
-}
-
-function Button(props) {
-	return <button onClick={props.onClick}>{props.textContent}</button>
-}
-
-class LoginControl extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { isLoggedIn: false }
-
-		this.handleLogIn = this.handleLogIn.bind(this)
-		this.handleLogOut = this.handleLogOut.bind(this)
-	}
-
-	handleLogIn() {
-		this.setState({ isLoggedIn: true })
-	}
-
-	handleLogOut() {
-		this.setState({ isLoggedIn: false })
-	}
-
-	render() {
-		const button = this.state.isLoggedIn ? (
-			<Button onClick={this.handleLogOut} textContent="Logout" />
-		) : (
-			<Button onClick={this.handleLogIn} textContent="Login" />
-		)
-		return (
-			<div>
-				<Greeting isLoggedIn={this.state.isLoggedIn} />
-				{button}
-			</div>
-		)
-	}
+function App() {
+	return (
+		<div>
+			<WelcomeDialog />
+		</div>
+	)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))

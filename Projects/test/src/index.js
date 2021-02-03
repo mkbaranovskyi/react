8dba8eabccdec63941d24e8b7bbda7e71dff889c
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2021-02-03 18:39:17 +0200

    basic tutorial finished

--- Projects/test/src/index.js
+++ Projects/test/src/index.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function FancyBorder(props) {
-	return (
-		<div className={'FancyBorder FancyBorder-' + props.color}>
-			{props.children}
-		</div>
-	)
-}
-
-function WelcomeDialog() {
-	return (
-		<FancyBorder color="blue">
-			<h1 className="Dialog-title">Welcome</h1>
-			<p className="Dialog-message">Thank you for visiting our spacecraft!</p>
-		</FancyBorder>
-	)
-}
-
 function App() {
 	return (
 		<div>
-			<WelcomeDialog />
+			<NameForm />
 		</div>
 	)
+}
+
+class NameForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { value: '' }
+
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSumbit = this.handleSumbit.bind(this)
+	}
+
+	// Without this, you won't be able to change the input value
+	handleChange(e) {
+		this.setState({ value: e.target.value })
+	}
+
+	handleSumbit(e) {
+		console.log(this.state.value)
+		e.target.elements.text.value = ''
+		e.preventDefault()
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSumbit}>
+				<label>
+					Name:
+					<input name="text" onChange={this.handleChange} />
+				</label>
+				<button type="submit">Submit</button>
+			</form>
+		)
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))

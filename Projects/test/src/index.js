75cf37db2406013394b05fee3f589b836941b231
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2021-02-09 15:22:13 +0200

    effect finished

--- Projects/test/src/index.js
+++ Projects/test/src/index.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-function Welcome(props) {
-	return <h1>Hello, {props.name}</h1>
+function Example() {
+	const [count, setCount] = useState(0)
+
+	// Similar to componentDidMount and componentDidUpdate
+	useEffect(() => {
+		// Update the document title using the browser API
+		document.title = `You clicked ${Date.now()} times`
+		return () => {
+			console.log(Date.now())
+		}
+	})
+
+	return (
+		<div>
+			<p>You clicked {Date.now()} times</p>
+			<button onClick={() => setCount(count + 1)}>Click me</button>
+		</div>
+	)
 }
-ReactDOM.render(<Welcome name="max" />, document.getElementById('root'))
 
 function App() {
 	return (
 		<div>
-			<Welcome />
+			<Example />
 		</div>
 	)
 }

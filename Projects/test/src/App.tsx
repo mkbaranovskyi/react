7f828a16d0befa1d4b793e8a5ed120a16e3f047b
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2021-02-23 20:18:25 +0200

    React TS started

--- Projects/test/src/App.tsx
+++ Projects/test/src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar } from './components/Navbar'
 import { TodoForm } from './components/TodoForm'
 
 const App: React.FC = () => {
+	const [tasks, setTasks] = useState([])
+
+	const addHandler = (title: string): void => {
+		console.log(title)
+	}
+
 	return (
 		<>
 			<Navbar />
 			<div className="container">
-				<TodoForm />
+				<TodoForm onAdd={addHandler} />
 			</div>
 		</>
 	)
--- Projects/test/src/components/TodoForm.tsx
+++ Projects/test/src/components/TodoForm.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export const TodoForm: React.FC = () => {
+interface TodoFormProps {
+	onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+	// const [title, setTitle] = useState<string>('')
+	const ref = useRef<HTMLInputElement>(null)
+
+	// const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+	// 	setTitle(event.target.value)
+	// }
+
+	const keyPressHandler = (
+		event: React.KeyboardEvent<HTMLInputElement>
+	): void => {
+		if (event.key === 'Enter') {
+			props.onAdd(ref.current!.value)
+			ref.current!.value = ''
+		}
+	}
+
 	return (
 		<div className="input-field mt2">
 			<label className="active">
 				Enter your task
-				<input placeholder="Buy milk" />
+				<input
+					placeholder="Buy milk"
+					ref={ref}
+					// value={title}
+					// onChange={changeHandler}
+					onKeyPress={keyPressHandler}
+				/>
 			</label>
+			{/* <button type="submit">Add task</button> */}
 		</div>
 	)
 }

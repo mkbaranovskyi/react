497fded13292c0261bf14aea021c27de20e1cb78
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2021-06-01 02:43:14 +0300

    Todo next

--- Projects/test/src/features/todos/AddTodo.tsx
+++ Projects/test/src/features/todos/AddTodo.tsx
@@ -1,27 +1,44 @@
 import React, { useState } from 'react'
 
+/**
+ * This custom (optional) hook holds the info for our input
+ * @param { string } defaultValue
+ * @returns { value, onChange, clear } - exactly the parameters we pass to our input
+ */
+function useInputValue(defaultValue = '') {
+	const [value, setValue] = useState(defaultValue)
+	return {
+		// This `bind` object is passed to the component as attributes - we don't just put everything in the returned object since the component doesn't need any extra clutter
+		bind: {
+			value,
+			onChange: (e: React.ChangeEvent) => {
+				const target = e.target as HTMLInputElement
+				setValue(target.value)
+			}
+		},
+		value: () => value,
+		clear: () => setValue('')
+	}
+}
+
 const AddTodo: React.FC<{ onCreate: Function }> = ({
 	onCreate: createTodo
 }) => {
-	const [value, setValue] = useState('')
-
-	const handleChange = (e: React.ChangeEvent) => {
-		const target = e.target as HTMLInputElement
-		setValue(target.value)
-	}
+	const input = useInputValue('') // an object with 2 fields
 
 	const handleSubmit = (e: React.FormEvent): void => {
 		e.preventDefault()
-		const trimmedValue: string = value.trim()
+		const trimmedValue: string = input.value().trim()
 		if (trimmedValue) {
 			createTodo(trimmedValue)
-			setValue('')
+			input.clear()
 		}
 	}
 
+	// An example of destructuring of the attributes - we 'spread' our object with properties
 	return (
 		<form className="addTodo" onSubmit={handleSubmit}>
-			<input value={value} onChange={handleChange} />
+			<input {...input.bind} />
 			<button>Add todo</button>
 		</form>
 	)
